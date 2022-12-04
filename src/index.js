e13affefe11ac21df021b5fd6cb7009a8467d2f9
Mukesh prajapati <94641465+Mukeshkingstar@users.noreply.github.com>
2022-12-04 15:14:12 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 var path = require('path');
+const PORT = process.env.PORT || 3000;
 const staticPath = path.join(__dirname,'../public');
 app.use(express.static(staticPath));
 app.set("view engine","hbs")
@@ -13,8 +14,8 @@ app.get('/code', function (req, res) {
   res.render('code');
 });
 
-app.listen(4200, function () {
-console.log('Example app listening on port 4200!');
+app.listen(PORT, function () {
+console.log('Example app listening on port ${PORT}');
 });
 
 //console.log(path.join(__dirname,'../public'))
